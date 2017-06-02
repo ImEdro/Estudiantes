ef6f5e352078908a94e0c644b78469474ac9062b
JACKY <JACKY@LAPTOP-AIN798BC>
2017-06-02 13:01:20 -0500

    arreglo3

--- src/main/java/Controlador/ChartServlet.java
+++ src/main/java/Controlador/ChartServlet.java
@@ -6,7 +6,6 @@
 package Controlador;
 
 import BD.Equipos;
-import Modelo.Equipo;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.io.IOException;
@@ -58,15 +57,19 @@ public class ChartServlet extends HttpServlet {
 		
         DefaultCategoryDataset dataset = new DefaultCategoryDataset();
         Equipos eq = new Equipos();
-        Equipo e=eq.buscar(id);
         
-        dataset.addValue(2, String.copyValueOf(e.getNombre()), "1");  
+        dataset.addValue(25.0, "Series 1", "Category 1");   
+        dataset.addValue(34.0, "Series 1", "Category 2");   
+        dataset.addValue(19.0, "Series 2", "Category 1");   
+        dataset.addValue(29.0, "Series 2", "Category 2");   
+        dataset.addValue(41.0, "Series 3", "Category 1");   
+        dataset.addValue(33.0, "Series 3", "Category 2");   
 
 		
         JFreeChart chart = ChartFactory.createBarChart3D(
-            "equipo",      // chart title
-            "Nombre ",               // domain axis label
-            "id",                  // range axis label
+            "3D Bar Chart Demo",      // chart title
+            "Category",               // domain axis label
+            "Value",                  // range axis label
             dataset,                  // data
             PlotOrientation.VERTICAL, // orientation
             true,                     // include legend
